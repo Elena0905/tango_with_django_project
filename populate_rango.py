4fc79853623a6e2239a7b84ae78c231c6b3aa9a6
Yunxin Gan <2431069g@student.gla.ac.uk>
2020-01-26 13:35:15 +0000

    eleventh 11.1

--- populate_rango.py
+++ populate_rango.py
@@ -34,11 +34,11 @@ def populate():
 
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat,cat_data['views'],cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
-            for c in Category.objects.all():
-                for p in Page.objects.filter(category=c):
+    for c in Category.objects.all():
+        for p in Page.objects.filter(category=c):
                     print(f'- {c}: {p}')
 
 def add_page(cat, title, url, views=0):
@@ -48,8 +48,10 @@ def add_page(cat, title, url, views=0):
      p.save()
      return p
 
-def add_cat(name,likes=0,views=0):
+def add_cat(name,likes,views):
      c = Category.objects.get_or_create(name=name,likes=likes,views=views)[0]
+     c.likes=likes
+     c.views=views
      c.save()
      return c
 if __name__ == '__main__':
