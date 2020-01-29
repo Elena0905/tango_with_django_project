b023788a56b28833a12647f2ff1e8e185bf62717
Yunxin Gan <2431069g@student.gla.ac.uk>
2020-01-29 22:22:38 +0000

    fourth 4.1 commit

--- rango/views.py
+++ rango/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Rango says hey there partner!"+'<a href="/rango/about/">About</a>')
-#def index(request):
-#    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-#    return render(request, 'rango/index.html', context=context_dict)
-# 
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+ 
 def about(request):
- return HttpResponse( "Rango says here is the about page."+'<a href="/rango/">Index</a>')
+ return render(request, 'rango/about.html')
 #from rango.models import Category
 #from rango.models import Page
 #from django.shortcuts import render
