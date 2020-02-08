d38d752ec64bc38e1bbb27e6923baab9d80093e8
Yunxin Gan <2431069g@student.gla.ac.uk>
2020-02-08 09:30:51 +0000

    Chapter 9 9.8 commit

--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,6 @@
 from django.template.defaultfilters import slugify
 from django.db import models
+from django.contrib.auth.models import User
 class Category(models.Model):
     NAME_MAX_LENGTH = 128
     name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
@@ -20,5 +21,11 @@ class Page(models.Model):
     views = models.IntegerField(default=0)
     def __str__(self):
         return self.title
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    def __str__(self):
+        return self.user.username
 
 
